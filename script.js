0a04e9b1cba5f9751fcf0c35b9dd4f1cc29f640d
EZEQUIEL TEIXEIRA SANTOS <57824564851@info.local>
2024-05-27 11:00:23 -0300

    feat: formulario cadastrado

--- script.js
+++ script.js
@@ -135,3 +135,60 @@ function renderizarCases() {
     containerCards.innerHTML = template;
 }
 
+function carregarCases() {
+    fetch("http://localhost:3000/cases")
+    .then((resposta) => resposta.json())
+    .then((dadosTratados) => {
+        console.log(dadosTratados)
+
+        listaCases = dadosTratados
+
+        renderizarCases()
+    })
+}
+
+function solicitarOrcamento(event) {
+    // Pegar os valores dos inputs
+    let valorNome = document.getElementById("campo-nome").value
+    let valorEmail = document.getElementById("campo-email").value
+    let valorDescricao = document.getElementById("campo-texto").value
+    
+    // Organizar os valores em um objeto
+    let dadosForm = {
+        nome: valorNome,
+        email: valorEmail,
+        descricao: valorDescricao
+    }
+
+    // Enviar a requisição para a API
+
+    //metodo HTTP POST - Criar/Create -> Cadastrar um novo registro (solicitacao)
+    fetch("http://localhost:3000/solicitacoes", {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dadosForm)
+    })
+
+        // CASO SUCESSO
+        .then(resposta => {
+            console.log(resposta)
+
+            // Limpar os inputs
+            document.querySelector("#contato form").reset()
+
+            // Mostrar um alert de sucesso
+            alert("Solicitação enviada com sucesso! 😉")
+        })
+
+        // CASO ERRO
+        .catch(erro => {
+            // Mostar um alert com erro
+            console.log(erro)
+            alert("Erro na requisição! 🫤")
+        })
+        
+        // previni que o formulário regarregue a página ao enviar
+        event.preventDefault()
+}
